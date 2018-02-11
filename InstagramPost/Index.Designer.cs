eac831e7502e91686b70acb108a9a3cbf287a726
Alex Ghinea <alex.ghinea@equilobe.com>
2018-02-11 15:47:21 +0200

    Include all files

---
+++ InstagramPost/Index.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace InstagramPost
+{
+    partial class Index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Post Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(146, 235);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "textBox1";
+            this.txtDebug.Size = new System.Drawing.Size(540, 248);
+            this.txtDebug.TabIndex = 1;
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 601);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.button1);
+            this.Name = "Index";
+            this.Text = "Index";
+            this.Load += new System.EventHandler(this.Index_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtDebug;
+    }
+}
+
---
+++ InstagramPost/Index.cs
@@ -0,0 +1,107 @@
+﻿using InstaSharp;
+using InstaSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstagramPost
+{
+    public partial class Index : Form
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void Index_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //load file opem
+            string imagePath = string.Empty;
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.CheckFileExists)
+            {
+                imagePath = openFileDialog1.FileName;
+            }
+            //upload to instagram
+            var uploader = new InstagramUploader("username", ConvertToSecureString("password"));
+            uploader.InvalidLoginEvent += InvalidLoginEvent;
+            uploader.ErrorEvent += ErrorEvent;
+            uploader.OnCompleteEvent += OnCompleteEvent;
+            uploader.OnLoginEvent += OnLoginEvent;
+            uploader.SuccessfulLoginEvent += SuccessfulLoginEvent;
+            uploader.OnMediaConfigureStarted += OnMediaConfigureStarted;
+            uploader.OnMediaUploadStartedEvent += OnMediaUploadStartedEvent;
+            uploader.OnMediaUploadeComplete += OnmediaUploadCompleteEvent;
+            uploader.UploadImage(imagePath, "#UploadedFromPC", true, true);
+            txtDebug.AppendText("Your DeviceID is " + InstaSharp.Properties.Settings.Default.deviceId + Environment.NewLine);
+        }
+
+        public SecureString ConvertToSecureString(string strPassword)
+        {
+            var secureStr = new SecureString();
+            if (strPassword.Length > 0)
+            {
+                foreach (var c in strPassword.ToCharArray()) secureStr.AppendChar(c);
+            }
+            return secureStr;
+        }
+
+        public void OnMediaUploadStartedEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("Attempting to upload image" + Environment.NewLine);
+        }
+
+        public void OnmediaUploadCompleteEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("The image was uploaded, but has not been configured yet." + Environment.NewLine);
+        }
+
+        public void OnMediaConfigureStarted(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("The image has started to be configured" + Environment.NewLine);
+        }
+
+        public void SuccessfulLoginEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("Logged in! " + ((LoggedInUserResponse)e).FullName + Environment.NewLine);
+        }
+
+        public void OnLoginEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("Event fired for login: " + ((NormalResponse)e).Message + Environment.NewLine);
+        }
+
+        public void OnCompleteEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("Image posted to Instagram, here are all the urls" + Environment.NewLine);
+            foreach (var image in ((UploadResponse)e).Images)
+            {
+                txtDebug.AppendText("Url: " + image.Url + Environment.NewLine);
+                txtDebug.AppendText("Width: " + image.Width + Environment.NewLine);
+                txtDebug.AppendText("Height: " + image.Height + Environment.NewLine);
+            }
+        }
+
+        public void ErrorEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("Error " + ((ErrorResponse)e).Message + Environment.NewLine);
+        }
+
+        public void InvalidLoginEvent(object sender, EventArgs e)
+        {
+            txtDebug.AppendText("Error while logging " + ((ErrorResponse)e).Message + Environment.NewLine);
+        }
+    }
+}
