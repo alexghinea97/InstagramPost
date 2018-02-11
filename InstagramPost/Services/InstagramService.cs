1c97371b92393a62f59f2cf9691a179515541786
Alex Ghinea <alex.ghinea@equilobe.com>
2018-02-11 15:35:56 +0200

    add InstagramService

---
+++ InstagramPost/Services/InstagramService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPost.Services
+{
+    public class InstagramService
+    {
+        //public SecureString ConvertToSecureString(string strPassword)
+        //{
+        //    var secureStr = new SecureString();
+        //    if (strPassword.Length > 0)
+        //    {
+        //        foreach (var c in strPassword.ToCharArray()) secureStr.AppendChar(c);
+        //    }
+        //    return secureStr;
+        //}
+
+        //public void OnMediaUploadStartedEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("Attempting to upload image" + Environment.NewLine);
+        //}
+
+        //public void OnmediaUploadCompleteEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("The image was uploaded, but has not been configured yet." + Environment.NewLine);
+        //}
+
+        //public void OnMediaConfigureStarted(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("The image has started to be configured" + Environment.NewLine);
+        //}
+
+        //public void SuccessfulLoginEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("Logged in! " + ((LoggedInUserResponse)e).FullName + Environment.NewLine);
+        //}
+
+        //public void OnLoginEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("Event fired for login: " + ((NormalResponse)e).Message + Environment.NewLine);
+        //}
+
+        //public void OnCompleteEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("Image posted to Instagram, here are all the urls" + Environment.NewLine);
+        //    foreach (var image in ((UploadResponse)e).Images)
+        //    {
+        //        txtDebug.AppendText("Url: " + image.Url + Environment.NewLine);
+        //        txtDebug.AppendText("Width: " + image.Width + Environment.NewLine);
+        //        txtDebug.AppendText("Height: " + image.Height + Environment.NewLine);
+        //    }
+        //}
+
+        //public void ErrorEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("Error " + ((ErrorResponse)e).Message + Environment.NewLine);
+        //}
+
+        //public void InvalidLoginEvent(object sender, EventArgs e)
+        //{
+        //    txtDebug.AppendText("Error while logging " + ((ErrorResponse)e).Message + Environment.NewLine);
+        //}
+    }
+}
